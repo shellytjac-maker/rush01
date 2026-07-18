676713682b04244224c239e5626db4216b011802
Htoo Aung Khant - <htaung@z2t6c3.42bangkok.com>
2026-07-18 16:34:45 +0700

    init

---
+++ check_sudoku.c
@@ -0,0 +1,12 @@
+int	check_sudoku(int grid[4][4], int pos) // solve
+{
+	int i;
+	int j;
+
+	int row, col, num;
+	row = pos / 4;
+	col = pos % 4;
+	i = 0;
+	num = 1;
+
+}
---
+++ check_visibility.c
@@ -0,0 +1,11 @@
+int	count_visible(int line[4])
+{
+	int	i;
+	int	max;
+	int	count;
+
+	i = 0;
+	max = 0;
+	count = 0;
+
+}
