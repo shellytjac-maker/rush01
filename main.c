d90aca315d8286a12240693f0a957ff730b1436b
Htoo Aung Khant - <htaung@z2t6c3.42bangkok.com>
2026-07-18 14:37:22 +0700

    init

--- main.c
+++ main.c
@@ -6,12 +6,12 @@ int	parse_input(char *str, int *clues);
 int	main(int argc, char **argv)
 {
 	int	clues[16];
+	int	grid[4][4] = {0};
 
-	// int	grid[4][4] = {0};
 	if (argc == 2)
 	{
 		parse_input(argv[1], clues);
-		printf("grid %ls", clues);
+		printf(" %ls", clues);
 	}
 	return (0);
 }
